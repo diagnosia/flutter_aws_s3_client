ded02e2d10163cdd2b6ef442f8d3ebf0e1b4e12f
Martin Kamleithner <martin+kamleithner@diagnosia.com>
2019-06-24 14:47:14 +0200

    add description in pubspec.yaml

--- lib/src/model/list_bucket_result_contents.dart
+++ lib/src/model/list_bucket_result_contents.dart
@@ -2,7 +2,6 @@ library list_bucket_result_contents;
 
 import 'dart:convert';
 
-import 'package:built_collection/built_collection.dart';
 import 'package:built_value/built_value.dart';
 import 'package:built_value/serializer.dart';
 
